1e5ff055748dadffd5d99cc8cbac7f35c72fd9e8
rtanmay <rtanmay05@gmail.com>
2018-01-21 00:49:09 +0530

    Deleted some files

--- 1.cpp
+++ 1.cpp
@@ -3,5 +3,9 @@ using namespace std;
 int main()
 {
    cout<<"Hello world, this is a c++ program!";
+   cout<<"enter two number-";
+   float a,b;
+   cin>>a>>b;
+   cout<<"this is ?"<<a*b;
    return 0;
 }
